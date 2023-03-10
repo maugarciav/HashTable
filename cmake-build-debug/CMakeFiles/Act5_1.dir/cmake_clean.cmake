1c62cf0cced7d3e164c7bdfe67c142df0c802de4
maugarcia <93749475+maugarciav@users.noreply.github.com>
2023-03-10 15:23:56 -0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Act5_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Act5_1"
+  "Act5_1.pdb"
+  "CMakeFiles/Act5_1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Act5_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
